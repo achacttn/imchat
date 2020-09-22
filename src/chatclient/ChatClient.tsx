41f51386dfa2a00e54d7b30e7242543183e62778
Scott <Scott@HELIOS.local>
2020-09-22 22:56:31 +1000

    basic ws connection and client structure

--- src/chatclient/ChatClient.tsx
+++ src/chatclient/ChatClient.tsx
@@ -1,13 +1,30 @@
 import React from "react";
+import WSHandler from './clientComponents/WSHandler';
+import style from './ChatClient.module.css';
 
 interface ChatClientProps {
     username: string;
 }
 
 const ChatClient = ({ username }: ChatClientProps) => {
+
     return (
-        <div>
-            {username}
+        <div className={style.ClientContainer}>
+            <WSHandler />
+            <div className={style.ClientFlexContainer}>
+                <div className={style.ChatAndUserContainer}>
+                    <div className={style.ClientChatContainer}>
+                        Chat
+                    </div>
+                    <div className={style.ClientUserListContainer}>
+                        Users
+                        {username}
+                    </div>
+                </div>
+                <div className={style.ClientInputContainer}>
+                    Input
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/chatclient/clientComponents/WSHandler.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+// interface WSHandlerProps {}
+
+const WSHandler = () => {
+
+    // React.useEffect(() => {
+    //     console.log('=== WSHandler.tsx ===');
+    //     let ws = new WebSocket("wss://m0v4iuwr09.execute-api.ap-southeast-2.amazonaws.com/dev1");
+    //     ws.onopen = event => {
+    //         console.log('Websocket open event: ', event);
+    //     };
+    //     ws.onclose = event => {
+    //         console.log('Webosocket close event: ', event);
+    //     };
+    //     ws.onerror = event => {
+    //         console.log('Websocket error: ', event);
+    //     };
+    //     ws.onmessage = event => {
+    //         console.log('Websocket message: ', event);
+    //     };
+    // }, [])
+
+    return null;
+}
+
+export default WSHandler;
